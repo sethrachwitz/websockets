393e8b8aeda6bc35dd28d657a077652d87cf5370
Seth Rachwitz <srachwitz@gmail.com>
2016-06-08 10:43:19 -0400

    Changed structure of PHP emit

--- 3_php_socketio/web/src/data.php
+++ 3_php_socketio/web/src/data.php
@@ -5,10 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($_POST['message']) {
         $emitter = new SocketIO\Emitter();
-        $emitter->emit('message', [
-            'event_type' => 'message',
-            'data' => $_POST['message']
-        ]);
+        $emitter->emit('message', $_POST['message']);
     }
 }
 
